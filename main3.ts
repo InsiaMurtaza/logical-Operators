b838f8163c6f00197e4a694802e0f937ce01b3af
InsiaMurtaza <165552743+InsiaMurtaza@users.noreply.github.com>
2024-04-16 17:49:01 +0500

    Add files via upload

---
+++ main3.ts
@@ -0,0 +1,20 @@
+// logical operators
+let myPercentage = 35;
+if (myPercentage >= 80 && myPercentage < 100) {
+    console.log("your grade is A+");
+}
+else if (myPercentage>= 70 && myPercentage < 80 ) {
+    console.log("your grade is A");
+}
+else if (myPercentage>= 60 && myPercentage<70) {
+    console.log("your grade is B");
+}
+else if(myPercentage >= 50 && myPercentage<60) {
+    console.log("your grade is C");
+}
+else if(myPercentage > 100) {
+    console.log("Invalid Marks");
+}
+else {
+    console.log("your grade is F");
+}
